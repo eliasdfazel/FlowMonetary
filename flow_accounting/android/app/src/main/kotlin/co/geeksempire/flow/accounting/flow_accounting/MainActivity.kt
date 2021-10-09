39c627387a5af89004f4fa3e565954e83c913e8a
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-10-09 08:35:41 -0800

    Initializing Project

---
+++ flow_accounting/android/app/src/main/kotlin/co/geeksempire/flow/accounting/flow_accounting/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.geeksempire.flow.accounting.flow_accounting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flow_accounting/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=X:\Xemonstration\Flutter\Setup\FlutterSdk"
+export "FLUTTER_APPLICATION_PATH=X:\Xlutter\FlowAccounting\flow_accounting"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
