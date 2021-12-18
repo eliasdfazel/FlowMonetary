d5ecdd67b5cbdba78b87c993a4ba32f184f12866
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-12-18 09:15:25 -0900

    Initializing Project User Interfaces

--- flow_accounting/lib/interface/new_document.dart
+++ flow_accounting/lib/interface/new_document.dart
@@ -1,7 +1,9 @@
+import 'package:flow_accounting/database/operations/inputs.dart';
+import 'package:flow_accounting/database/structures/financial_reports.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp( home: MyApp(),));
+  runApp(const MaterialApp( home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -12,8 +14,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  TextEditingController textEditorController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    // textEditorController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Flutter TextField Example'),
@@ -22,32 +37,43 @@ class _State extends State<MyApp> {
             padding: const EdgeInsets.all(15),
             child: Column(
               children: <Widget>[
-                const Padding(
+                Padding(
                   padding: EdgeInsets.all(15),
                   child: TextField(
+                    controller: textEditorController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'User Name',
+                      labelText: 'Full Name',
                       hintText: 'Enter Your Name',
                     ),
                   ),
                 ),
-                const Padding(
+                Padding(
                   padding: EdgeInsets.all(15),
                   child: TextField(
-                    obscureText: true,
+                    obscureText: false,
+                    controller: textEditorController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter Password',
+                      labelText: 'Email Address',
+                      hintText: 'Enter Email Address',
                     ),
                   ),
                 ),
                 RaisedButton(
                   textColor: Colors.white,
                   color: Colors.blue,
-                  child: const Text('Sign In'),
-                  onPressed: (){},
+                  child: const Text('Saved'),
+                  onPressed: (){
+
+                    var databaseInputs = DatabaseInputs();
+
+                    databaseInputs.insertFinancialReport(FinancialReports(id:
+                    666, name: textEditorController.text, type: 1));
+
+                    print(">>> >> > " + textEditorController.text);
+                    
+                  },
                 )
               ],
             )
