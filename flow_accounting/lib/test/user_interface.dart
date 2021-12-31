14579348068e7ffc8ee9e5a4684e036ac1bc4ed8
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-12-31 01:59:57 -0900

    Initializing Project Structure

--- flow_accounting/lib/test/user_interface.dart
+++ flow_accounting/lib/test/user_interface.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() {runApp(MyApp());}
 
 class Choice {
   const Choice({required this.title, required this.icon});
-
   final String title;
   final IconData icon;
 }
 
+
+
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     List<Choice> choices = <Choice>[
       const Choice(title: 'Home', icon: Icons.home),
       const Choice(title: 'Contact', icon: Icons.contacts),
@@ -26,10 +27,9 @@ class MyApp extends StatelessWidget {
     ];
 
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Flutter GridView Demo"),
-            ),
+        home: Scaffold(appBar: AppBar(
+          title: Text("Flutter GridView Demo"),
+        ),
             body: GridView.count(
                 crossAxisCount: 3,
                 crossAxisSpacing: 4.0,
@@ -38,9 +38,15 @@ class MyApp extends StatelessWidget {
                   return Center(
                     child: SelectCard(choice: choices[index]),
                   );
-                }))));
+                })
+            )
+        )
+    );
   }
+
 }
+
+
 
 class SelectCard extends StatelessWidget {
   const SelectCard({Key? key, required this.choice}) : super(key: key);
@@ -49,19 +55,19 @@ class SelectCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     final TextStyle? textStyle = Theme.of(context).textTheme.bodyText1;
 
     return Card(
         color: Colors.orange,
-        child: Center(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                    child:
-                        Icon(choice.icon, size: 50.0, color: textStyle!.color)),
-                Text(choice.title, style: textStyle),
-              ]),
-        ));
+        child: Center(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(child: Icon(choice.icon, size:50.0, color: textStyle!.color)),
+              Text(choice.title, style: textStyle),
+            ]
+        ),
+        )
+    );
   }
 }
