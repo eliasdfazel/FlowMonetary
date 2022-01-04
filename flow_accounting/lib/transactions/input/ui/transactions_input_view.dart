de9f30ec77aa226b004b8cb48e581c5683ee6e31
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-01-04 07:58:28 -0900

    Initializing Project Structure

--- flow_accounting/lib/transactions/input/ui/transactions_input_view.dart
+++ flow_accounting/lib/transactions/input/ui/transactions_input_view.dart
@@ -27,97 +27,101 @@ class _State extends State<TransactionsView> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-        body: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child:  Text(
-                          'Transactions',
-                          style: TextStyle(
-                            fontSize: 32,
-                            color: Colors.black87,
-                            shadows: [
-                              Shadow(
-                                color: Colors.black.withOpacity(0.5),
-                                offset: Offset(3, 3),
-                                blurRadius: 7,
-                              )
-                            ],
+    return SafeArea(child: MaterialApp(
+      home: Scaffold(
+          body: Column (
+            children: [
+              Expanded(child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Expanded(child: ListView(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child:  Text(
+                            'Transactions',
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Colors.black87,
+                              shadows: [
+                                Shadow(
+                                  color: Colors.black.withOpacity(0.5),
+                                  offset: Offset(3, 3),
+                                  blurRadius: 7,
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: TextField(
-                          controller: textEditorControllerName,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Full Name',
-                            hintText: 'Enter Your Name',
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: TextField(
+                            controller: textEditorControllerName,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Full Name',
+                              hintText: 'Enter Your Name',
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 7,
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: TextField(
-                          obscureText: false,
-                          controller: textEditorControllerEmail,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Email Address',
-                            hintText: 'Enter Email Address',
+                      Expanded(
+                        flex: 7,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: TextField(
+                            obscureText: false,
+                            controller: textEditorControllerEmail,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Email Address',
+                              hintText: 'Enter Email Address',
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                ],)),
+              )),
+              MaterialButton(
+                textColor: Colors.white,
+                color: Colors.blue,
+                child: const Text('Insert'),
+                onPressed: () {
+
+                  var databaseInputs = DatabaseInputs();
+
+                  final snackBar = SnackBar(
+                    content: Text("${textEditorControllerName.text} |  ${textEditorControllerEmail.text}"),
+                    action: SnackBarAction(
+                      label: 'OK',
+                      onPressed: () {
+
+                        // databaseInputs.insertFinancialReport(TransactionsData(id: 666, sourceCardNumber: textEditorControllerName.text, type: 1));
+
+                      },
                     ),
-                  ],
-                ),
-                MaterialButton(
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  child: const Text('Insert'),
-                  onPressed: (){
+                  );
 
-                    var databaseInputs = DatabaseInputs();
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 
-                    final snackBar = SnackBar(
-                      content: Text("${textEditorControllerName.text} |  ${textEditorControllerEmail.text}"),
-                      action: SnackBarAction(
-                        label: 'OK',
-                        onPressed: () {
-
-                          // databaseInputs.insertFinancialReport(TransactionsData(id: 666, sourceCardNumber: textEditorControllerName.text, type: 1));
-
-                        },
-                      ),
-                    );
-
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    
-                  },
-                )
-              ],
-            )
-        )
-    );
+                },
+              )
+            ],
+          )
+      ),
+    ));
   }
 }
---
+++ flow_accounting/lib/transactions/output/ui/transactions_output_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp( home: TransactionsView()));
+}
+
+class TransactionsView extends StatefulWidget {
+  const TransactionsView({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<TransactionsView> {
+
+  TextEditingController textEditorControllerName = TextEditingController();
+  TextEditingController textEditorControllerEmail = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    // textEditorController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(child: MaterialApp(
+      home: Scaffold(
+          body: CustomScrollView(
+            slivers: <Widget>[
+              const SliverAppBar(
+                pinned: true,
+                expandedHeight: 250.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text('Demo'),
+                ),
+              ),
+              SliverGrid(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200.0,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  childAspectRatio: 4.0,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.teal[100 * (index % 9)],
+                      child: Text('Grid Item $index'),
+                    );
+                  },
+                  childCount: 20,
+                ),
+              ),
+              SliverFixedExtentList(
+                itemExtent: 50.0,
+                delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.lightBlue[100 * (index % 9)],
+                      child: Text('List Item $index'),
+                    );
+                  },
+                ),
+              ),
+            ],
+          )
+      ),
+    ));
+  }
+}
