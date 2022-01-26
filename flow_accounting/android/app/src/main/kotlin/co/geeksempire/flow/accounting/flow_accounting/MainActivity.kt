bf7e6b94fb62ab18e4d8cc2fe2e12caea763f26d
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-01-26 03:30:40 -0900

    Input | Transactions { - Initializing Input Views } Security Options { - Disabled Screenshots }

--- flow_accounting/android/app/src/main/kotlin/co/geeksempire/flow/accounting/flow_accounting/MainActivity.kt
+++ flow_accounting/android/app/src/main/kotlin/co/geeksempire/flow/accounting/flow_accounting/MainActivity.kt
@@ -3,4 +3,8 @@ package co.geeksempire.flow.accounting.flow_accounting
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
