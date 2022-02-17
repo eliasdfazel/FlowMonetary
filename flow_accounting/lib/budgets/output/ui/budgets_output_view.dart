d8a9a9edf28e3efafaf22c4dfe954ccdbf4ba9bf
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-02-17 06:56:16 -0900

    Budget Management { - Input }

--- flow_accounting/lib/budgets/output/ui/budgets_output_view.dart
+++ flow_accounting/lib/budgets/output/ui/budgets_output_view.dart
@@ -6,7 +6,9 @@ import 'package:flow_accounting/budgets/database/structures/tables_structure.dar
 import 'package:flow_accounting/budgets/input/ui/budgets_input_view.dart';
 import 'package:flow_accounting/resources/ColorsResources.dart';
 import 'package:flow_accounting/resources/StringsResources.dart';
+import 'package:flow_accounting/transactions/output/ui/transactions_output_view.dart';
 import 'package:flow_accounting/utils/colors/color_selector.dart';
+import 'package:flow_accounting/utils/navigations/navigations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:marquee/marquee.dart';
@@ -420,6 +422,24 @@ class _BudgetOutputView extends State<BudgetsOutputView> {
             foregroundColor: ColorsResources.applicationGeeksEmpire,
             icon: Icons.edit_rounded,
             label: StringsResources.editText,
+            autoClose: true,
+          ),
+        ],
+      ),
+      startActionPane: ActionPane(
+        motion: const DrawerMotion(),
+        children: [
+          SlidableAction(
+            flex: 1,
+            onPressed: (BuildContext context) {
+
+              NavigationProcess().goTo(context, TransactionsOutputView(initialSearchQuery: budgetName));
+
+            },
+            backgroundColor: Colors.transparent,
+            foregroundColor: ColorsResources.greenGray,
+            icon: Icons.money_rounded,
+            label: StringsResources.transactionAll,
             autoClose: true,
           ),
         ],
