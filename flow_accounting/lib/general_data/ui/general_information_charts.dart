b098089e45b74d3a2df5e9759dd85e2054d27b7e
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-02-24 10:15:10 -0900

    Added Validation Checkpoints for Submit { - Transactions - Credit Cards - Budgets }

--- flow_accounting/lib/general_data/ui/general_information_charts.dart
+++ flow_accounting/lib/general_data/ui/general_information_charts.dart
@@ -38,54 +38,54 @@ class _GeneralFinancialChartsState extends State<GeneralFinancialCharts> {
     // Convert Monthly Money Amount to be Between
 
     List<double> listOfEarningPoint = [
-      70,
-      30,
-      20,
-      50,
-      35.1,
-      40,
-      30,
-      19,
-      29,
-      41,
-      80,
-      18,
+      700000,
+      300000000,
+      200,
+      500000000000,
+      350000.1,
+      4000000000,
+      30000000000000000,
+      1900000,
+      290000,
+      40001,
+      80000,
+      1800000,
     ];
     double minimumEarning = listOfEarningPoint.reduce((current, next) => (current < next) ? current : next);
     double maximumEarning = listOfEarningPoint.reduce((current, next) => (current > next) ? current : next);
     LineChartView generalEarningChart = LineChartView(listOfSpotY: listOfEarningPoint, minimumY: minimumEarning, maximumY: maximumEarning);
 
     List<double> listOfSpendingPoint = [
-      30,
-      30,
-      20,
-      50,
-      35.1,
-      40,
-      30,
-      49,
-      49,
-      41,
-      40,
-      18,
+      300000,
+      3000000000,
+      20000,
+      500,
+      3500.1,
+      400,
+      3000000,
+      490,
+      49000,
+      410,
+      4000,
+      180,
     ];
     double minimumSpending = listOfEarningPoint.reduce((current, next) => (current < next) ? current : next);
     double maximumSpending = listOfEarningPoint.reduce((current, next) => (current > next) ? current : next);
     LineChartView generalSpendingChart = LineChartView(listOfSpotY: listOfSpendingPoint, minimumY: minimumSpending, maximumY: maximumSpending);
 
     List<double> listOfBalancePoint = [
-      10,
-      30,
-      20,
-      50,
-      65.1,
-      40,
-      20,
-      49,
-      90,
-      41,
-      10,
-      88,
+      1000,
+      3000,
+      200,
+      50000000,
+      6500.1,
+      400,
+      200,
+      490000000,
+      900,
+      410,
+      100,
+      8800000,
     ];
     double minimumBalance = listOfEarningPoint.reduce((current, next) => (current < next) ? current : next);
     double maximumBalance = listOfEarningPoint.reduce((current, next) => (current > next) ? current : next);
