f12331a4742b6118fb7acfb27f9e9cbd1bb2e426
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-03-13 08:42:05 -0800

    Profile Database { - Profile Input View }

--- flow_accounting/lib/profile/database/io/inputs.dart
+++ flow_accounting/lib/profile/database/io/inputs.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 3/13/22, 8:38 AM
+ * Last modified 3/13/22, 8:42 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -40,7 +40,7 @@ class ProfilesDatabaseInputs {
         );
       },
 
-      version: 101,
+      version: 1,
     );
 
     final databaseInstance = await database;
