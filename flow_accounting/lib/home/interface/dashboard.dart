f77e143bf881f7a5d12ef23fb5f3e122ae639dc8
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-04-09 07:15:59 -0800

    Authentication Process Added

--- flow_accounting/lib/home/interface/dashboard.dart
+++ flow_accounting/lib/home/interface/dashboard.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 4/9/22, 6:52 AM
+ * Last modified 4/9/22, 7:15 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -76,7 +76,7 @@ class FlowDashboardState extends State<FlowDashboard> {
 
 
           })
-      ),
+      )
     );
 
     if (!kDebugMode) {
