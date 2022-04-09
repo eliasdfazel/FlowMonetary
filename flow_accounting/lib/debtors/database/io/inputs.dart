f4f668ccbf3152f63e9bbe593367614010c16632
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-04-09 08:03:14 -0800

    Authentication Process Added

---
+++ flow_accounting/lib/debtors/database/io/inputs.dart
@@ -0,0 +1,96 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/9/22, 8:03 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import 'package:flow_accounting/debtors/database/structures/tables_structure.dart';
+import 'package:flow_accounting/profile/database/io/queries.dart';
+import 'package:flow_accounting/resources/StringsResources.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DebtorsDatabaseInputs {
+
+  static const String databaseTableName = "all_debtors";
+
+  static String debtorsDatabase() {
+
+    return (UserInformation.UserId == StringsResources.unknownText()) ? "debtors_database.db" : "${UserInformation.UserId}_debtors_database.db";;
+  }
+
+  Future<void> insertDebtorData(DebtorsData debtorsData, String tableName,
+      String usernameId, {bool isPrototype = false}) async {
+
+    var databaseNameQuery = DebtorsDatabaseInputs.debtorsDatabase();
+    var tableNameQuery = DebtorsDatabaseInputs.databaseTableName;
+
+    final database = openDatabase(
+        join(await getDatabasesPath(), databaseNameQuery),
+        onCreate: (databaseInstance, version) {
+
+          return databaseInstance.execute(
+            'CREATE TABLE IF NOT EXISTS $tableNameQuery(id INTEGER PRIMARY KEY, '
+                'debtorsName TEXT, '
+                'debtorsDescription TEXT, '
+                'debtorsCompleteDebt TEXT, '
+                'debtorsPaidDebt TEXT, '
+                'debtorsRemainingDebt TEXT, '
+                'debtorsDeadline TEXT, '
+                'debtorsDeadlineText TEXT, '
+                'colorTag TEXT'
+                ')',
+          );
+        },
+
+        version: 1,
+        readOnly: false
+    );
+
+    final databaseInstance = await database;
+
+    await databaseInstance.insert(
+      tableNameQuery,
+      debtorsData.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    if (databaseInstance.isOpen && !isPrototype) {
+
+      await databaseInstance.close();
+
+    }
+
+  }
+
+  Future<void> updateBudgetData(DebtorsData debtorsData, String tableName, String usernameId) async {
+
+    var databaseNameQuery = DebtorsDatabaseInputs.debtorsDatabase();
+    var tableNameQuery = DebtorsDatabaseInputs.databaseTableName;
+
+    final database = openDatabase(
+      join(await getDatabasesPath(), databaseNameQuery),
+    );
+
+    final databaseInstance = await database;
+
+    await databaseInstance.update(
+      tableNameQuery,
+      debtorsData.toMap(),
+      where: 'id = ?',
+      whereArgs: [debtorsData.id],
+    );
+
+    if (databaseInstance.isOpen) {
+
+      await databaseInstance.close();
+
+    }
+
+  }
+
+}
---
+++ flow_accounting/lib/debtors/database/structures/tables_structure.dart
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/9/22, 7:58 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import 'package:flow_accounting/resources/ColorsResources.dart';
+
+class DebtorsData {
+
+  final int id;
+
+  final String debtorsName;
+  final String debtorsDescription;
+
+  final String debtorsCompleteDebt;
+
+  final String debtorsPaidDebt;
+  final String debtorsRemainingDebt;
+
+  final String debtorsDeadline;
+  final String debtorsDeadlineText;
+
+  int colorTag = ColorsResources.dark.value;
+
+  DebtorsData({
+    required this.id,
+
+    required this.debtorsName,
+    required this.debtorsDescription,
+
+    required this.debtorsCompleteDebt,
+
+    required this.debtorsPaidDebt,
+    required this.debtorsRemainingDebt,
+
+    required this.debtorsDeadline,
+    required this.debtorsDeadlineText,
+
+    required this.colorTag,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+
+      'debtorsName': debtorsName,
+      'debtorsDescription': debtorsDescription,
+
+      'debtorsCompleteDebt': debtorsCompleteDebt,
+
+      'debtorsPaidDebt': debtorsPaidDebt,
+      'debtorsRemainingDebt': debtorsRemainingDebt,
+
+      'debtorsDeadline': debtorsDeadline,
+      'debtorsDeadlineText': debtorsDeadlineText,
+
+      'colorTag': colorTag,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'DebtorsData{'
+      'id: $id,'
+
+        'debtorsName: $debtorsName,'
+        'debtorsDescription: $debtorsDescription,'
+
+        'debtorsCompleteDebt: $debtorsCompleteDebt,'
+
+        'debtorsPaidDebt: $debtorsPaidDebt,'
+        'debtorsRemainingDebt: $debtorsRemainingDebt,'
+
+        'debtorsDeadline: $debtorsDeadline,'
+        'debtorsDeadlineText: $debtorsDeadlineText,'
+
+      'colorTag: $colorTag,'
+    '}';
+  }
+}
