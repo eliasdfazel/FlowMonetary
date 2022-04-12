0730fa92cb947e8b0eb14eb78104a733188f1b56
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-04-12 05:21:50 -0800

    Loans Database

---
+++ flow_accounting/lib/loans/database/io/inputs.dart
@@ -0,0 +1,95 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/12/22, 5:21 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import 'package:flow_accounting/loans/database/structure/tables_structure.dart';
+import 'package:flow_accounting/profile/database/io/queries.dart';
+import 'package:flow_accounting/resources/StringsResources.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class LoansDatabaseInputs {
+
+  static const String databaseTableName = "all_cheques";
+
+  static String loansDatabase() {
+
+    return (UserInformation.UserId == StringsResources.unknownText()) ? "loans_database.db" : "${UserInformation.UserId}_loans_database.db";
+  }
+
+  Future<void> insertLoanData(LoansData loansData, String tableName,
+      String usernameId) async {
+
+    var databaseNameQuery = LoansDatabaseInputs.loansDatabase();
+    var tableNameQuery = LoansDatabaseInputs.databaseTableName;
+
+    final database = openDatabase(
+        join(await getDatabasesPath(), databaseNameQuery),
+        onCreate: (databaseInstance, version) {
+
+          return databaseInstance.execute(
+            'CREATE TABLE IF NOT EXISTS $tableNameQuery(id INTEGER PRIMARY KEY, '
+                'loanDescription TEXT, '
+                'loanIssueMonthly TEXT, '
+                'loanIssueMonthlyMillisecond TEXT, '
+                'loanComplete TEXT, '
+                'loanPaid TEXT, '
+                'loanRemaining TEXT, '
+                'colorTag TEXT'
+                ')',
+          );
+        },
+
+        version: 1,
+        readOnly: false
+    );
+
+    final databaseInstance = await database;
+
+    await databaseInstance.insert(
+      tableNameQuery,
+      loansData.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    if (databaseInstance.isOpen) {
+
+      await databaseInstance.close();
+
+    }
+
+  }
+
+  Future<void> updateChequeData(LoansData loansData, String tableName, String usernameId) async {
+
+    var databaseNameQuery = LoansDatabaseInputs.loansDatabase();
+    var tableNameQuery = LoansDatabaseInputs.databaseTableName;
+
+    final database = openDatabase(
+      join(await getDatabasesPath(), databaseNameQuery),
+    );
+
+    final databaseInstance = await database;
+
+    await databaseInstance.update(
+      tableNameQuery,
+      loansData.toMap(),
+      where: 'id = ?',
+      whereArgs: [loansData.id],
+    );
+
+    if (databaseInstance.isOpen) {
+
+      await databaseInstance.close();
+
+    }
+
+  }
+
+}
---
+++ flow_accounting/lib/loans/database/io/queries.dart
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/12/22, 5:21 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import 'package:flow_accounting/loans/database/io/inputs.dart';
+import 'package:flow_accounting/loans/database/structure/tables_structure.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class LoansDatabaseQueries {
+
+  Future<List<LoansData>> getAllLoans(String tableName,
+      String usernameId) async {
+
+    var databaseNameQuery = LoansDatabaseInputs.loansDatabase();
+    var tableNameQuery = LoansDatabaseInputs.databaseTableName;
+
+    final database = openDatabase(
+      join(await getDatabasesPath(), databaseNameQuery),
+    );
+
+    final databaseInstance = await database;
+
+    final List<Map<String, dynamic>> maps = await databaseInstance.query(tableNameQuery);
+
+    return List.generate(maps.length, (i) {
+      return LoansData(
+        id: maps[i]['id'],
+
+        loanDescription: maps[i]['loanDescription'],
+
+        loanIssueMonthly: maps[i]['loanIssueMonthly'],
+        loanIssueMonthlyMillisecond: maps[i]['loanIssueMonthlyMillisecond'],
+
+        loanComplete: maps[i]['loanComplete'],
+        loanPaid: maps[i]['loanPaid'],
+        loanRemaining: maps[i]['loanRemaining'],
+
+        colorTag: int.parse(maps[i]['colorTag'].toString()),
+      );
+    });
+
+  }
+
+  Future<int> queryDeleteCheque(int id,
+      String tableName, String usernameId) async {
+
+    var databaseNameQuery = LoansDatabaseInputs.loansDatabase();
+    var tableNameQuery = LoansDatabaseInputs.databaseTableName;
+
+    final database = openDatabase(
+      join(await getDatabasesPath(), databaseNameQuery),
+    );
+
+    final databaseInstance = await database;
+
+    var queryResult = await databaseInstance.delete(
+      tableNameQuery,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    return queryResult;
+  }
+
+}
