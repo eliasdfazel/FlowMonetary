7d5e4fa13d98cbcb39d4b1836b39a644f2e584d6
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-05-07 09:53:36 -0800

    - Offline Backup Initialize - Partial Advanced Search for Cheques Added - Advanced Search for Cheques Added - Partial Advanced Search for Transactions Added - Advanced Search for Transactions Added - List Of Loans Payments - Cheque Category Added - Invoice Buy/Sell QR Barcode - Invoice Buy/Sell Return Partial - Invoice Buy/Sell Return Complete - Adding More Products for Invoices { - Auto Calculate Price & etc. } - Shipping Expenses for Buy/Sell Invoices - Calendar Initial Text for All Inputs Forms - Number Formatter for All Inputs Money - Digital Signature { - Sell/Buy Invoices } - Sell/Buy Invoices, Product { - Unit - Tax Added } - Quick Action: Transactions Added

--- flow_accounting/ios/Runner/GeneratedPluginRegistrant.m
+++ flow_accounting/ios/Runner/GeneratedPluginRegistrant.m
@@ -12,6 +12,12 @@
 @import add_2_calendar;
 #endif
 
+#if __has_include(<flutter_archive/FlutterArchivePlugin.h>)
+#import <flutter_archive/FlutterArchivePlugin.h>
+#else
+@import flutter_archive;
+#endif
+
 #if __has_include(<flutter_barcode_scanner/SwiftFlutterBarcodeScannerPlugin.h>)
 #import <flutter_barcode_scanner/SwiftFlutterBarcodeScannerPlugin.h>
 #else
@@ -88,6 +94,7 @@
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [Add2CalendarPlugin registerWithRegistrar:[registry registrarForPlugin:@"Add2CalendarPlugin"]];
+  [FlutterArchivePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterArchivePlugin"]];
   [SwiftFlutterBarcodeScannerPlugin registerWithRegistrar:[registry registrarForPlugin:@"SwiftFlutterBarcodeScannerPlugin"]];
   [FlutterKeyboardVisibilityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterKeyboardVisibilityPlugin"]];
   [FlutterNativeSplashPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterNativeSplashPlugin"]];
